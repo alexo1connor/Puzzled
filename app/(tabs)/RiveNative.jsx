df4d6307ecb44c0474fdd857adac0f2b584b76ec
x00200430 <x00200430@mytudublin.ie>
2025-09-29 15:19:24 +0100

    New YML file & Rive Fix 2

---
+++ app/(tabs)/RiveNative.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, View } from 'react-native';
+import Rive from 'rive-react-native';
+
+function RiveDemo() {
+  return <Rive
+      url="https://public.rive.app/community/runtime-files/2195-4346-avatar-pack-use-case.riv"
+      artboardName="Avatar 1"
+      stateMachineName="avatar"
+      style={{width: 400, height: 400}}
+  />;
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <RiveDemo />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
